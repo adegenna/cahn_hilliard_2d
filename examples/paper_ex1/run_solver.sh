aff40f117be10f17ae0152b5f5032462703696e0
Anthony DeGennaro <amdegennaro@gmail.com>
2020-06-02 19:05:39 -0400

    Small updates to paper_ex1

--- examples/paper_ex1/run_solver.sh
+++ examples/paper_ex1/run_solver.sh
@@ -1,10 +1,12 @@
 
-export build_dir="../../build/"
+export basedir="/home/adegennaro/Projects/AEOLUS/cahn_hilliard_2d/examples/paper_ex1/"
+
+export build_dir=${basedir}"../../build/"
 export mpiexec_petsc=${build_dir}"external/petsc/linux-gnu-c-debug/bin/mpiexec"
 
 ### RUN THE ACTUAL CH SOLVER
 
-export petsc_inputfile="./petscrc.dat"
+export petsc_inputfile=${basedir}"petscrc.dat"
 export petsc_solver=${build_dir}"ch2d"
 
 echo "PETSc inputfile =" $petsc_inputfile
